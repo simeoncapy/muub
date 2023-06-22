d026be817213a0fcf377e824d4e1f7373db2b62d
Siméon <8012383+simeoncapy@users.noreply.github.com>
2023-06-23 00:02:48 +0900

    Creation of the layout

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,19 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->time->setText(QTime::currentTime().toString("hh:mm"));
+    startTimer(1000);
+
+    QFont fontCategory = QFont("Helvetica", 16, QFont::Bold);
+    ui->mediaCategory->setFont(fontCategory);
+    ui->videoGamesCategory->setFont(fontCategory);
+    ui->time->setFont(fontCategory);
+
+    QPixmap logo(":/images/logo.svg");
+    //QIcon(":/images/logo.svg").pixmap(QSize())
+    ui->logo->setPixmap(QIcon(":/images/logo.svg").pixmap(QSize()));
+    //ui->logo->setMask(logo.mask());
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +26,7 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::timerEvent(QTimerEvent * event)
+{
+    ui->time->setText(QTime::currentTime().toString("hh:mm"));
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTime>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +18,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+    void timerEvent(QTimerEvent *event);
 };
 #endif // MAINWINDOW_H
